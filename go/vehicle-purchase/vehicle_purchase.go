96d23029f63a414579f362451915f757143a7c37
Kater <git@ktrtechlabs.com>
2023-06-12 10:31:14 +1000

    added solutuion to exercise

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,32 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" {
+		return true
+	} else if kind == "truck" {
+		return true
+	} else {
+		return false
+	}
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	comment := " is clearly the better choice."
+	if option1 < option2 {
+		return option1 + comment
+	} else {
+		return option2 + comment
+	}
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age >= 10 {
+		return originalPrice * 0.5
+	} else {
+		return originalPrice * 0.7
+	}
 }
