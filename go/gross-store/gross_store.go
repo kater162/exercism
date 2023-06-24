4d06e17ca3a0782f6492e62871fc61b9d9853cdf
Kater <git@ktrtechlabs.com>
2023-06-24 16:03:04 +1000

    Added solution to exercise

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -2,25 +2,62 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	storeUnits := map[string]int{}
+	storeUnits["quarter_of_a_dozen"] = 3
+	storeUnits["half_of_a_dozen"] = 6
+	storeUnits["dozen"] = 12
+	storeUnits["small_gross"] = 120
+	storeUnits["gross"] = 144
+	storeUnits["great_gross"] = 1728
+
+	return storeUnits
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+	emptyBill := make(map[string]int)
+	return emptyBill
 }
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the AddItem() function")
+	_, exists := units[unit]
+	if !exists {
+		return false
+	}
+	_, e := bill[item]
+	if e {
+		bill[item] = bill[item] + units[unit]
+		return true
+	} else {
+		bill[item] = units[unit]
+		return true
+	}
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	_, a := bill[item]
+	_, b := units[unit]
+
+	if !a || !b {
+		return false
+	}
+
+	switch {
+	case (bill[item] - units[unit]) < 0:
+	return false
+	case (bill[item] - units[unit]) == 0:
+	delete(bill, item)
+	return true
+	default:
+	bill[item] = bill[item] - units[unit]
+	return true
+	}
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	quantity, exists := bill[item]
+	return quantity, exists
 }
