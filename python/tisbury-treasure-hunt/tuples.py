48384ecfe80a56c2c98905081f6268f5d6094bf3
Kater <git@ktrtechlabs.com>
2023-12-11 20:48:51 +1100

    Added solution to tuples exercis

--- python/tisbury-treasure-hunt/tuples.py
+++ python/tisbury-treasure-hunt/tuples.py
@@ -8,7 +8,7 @@ def get_coordinate(record):
     :return: str - the extracted map coordinate.
     """
 
-    pass
+    return record[1]
 
 
 def convert_coordinate(coordinate):
@@ -18,7 +18,7 @@ def convert_coordinate(coordinate):
     :return: tuple - the string coordinate split into its individual components.
     """
 
-    pass
+    return tuple(coordinate)
 
 
 def compare_records(azara_record, rui_record):
@@ -29,7 +29,9 @@ def compare_records(azara_record, rui_record):
     :return: bool - do the coordinates match?
     """
 
-    pass
+    converted = convert_coordinate(azara_record[1])
+
+    return converted == rui_record[1]
 
 
 def create_record(azara_record, rui_record):
@@ -40,7 +42,9 @@ def create_record(azara_record, rui_record):
     :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
     """
 
-    pass
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    return "not a match"
 
 
 def clean_up(combined_record_group):
@@ -53,5 +57,10 @@ def clean_up(combined_record_group):
 
     (see HINTS.md for an example).
     """
+    
+    report = ""
+    for entry in combined_record_group:
+        new_t = tuple([entry[0], entry[2], entry[3], entry[4]])
+        report += str(new_t) + "\n"
 
-    pass
+    return report
